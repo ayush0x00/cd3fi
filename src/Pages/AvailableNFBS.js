49b117351d07105f2f6a019db5e47bd946369a02
Ayush Tiwari <ayushtiwari5747462@gmail.com>
2022-07-19 17:40:15 +0530

    Data fetch bug

--- src/Pages/AvailableNFBS.js
+++ src/Pages/AvailableNFBS.js
@@ -1,59 +1,76 @@
-import React from 'react'
+import React from "react";
 
-import Card from '../components/Sales/Card'
-import certificate from '../Media/Cartificate-Dark.png'
-import { useState, useEffect } from 'react'
-import ReactPaginate from 'react-paginate'
-import { data } from '../fakeData'
+import Card from "../components/Sales/Card";
+import Dark from "../Media/Cartificate-Dark.png";
+import Light from "../Media/NFB Certificate (3)@2x.png";
+import { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
 
 export default function AvailableNFBS() {
-  {/* Import your own fetched data for the items */ }
-  const items = data
+  {
+    /* Import your own fetched data for the items */
+  }
+  const [items, setItems] = useState([{}]);
 
   const itemsPerPage = 8;
-  const [currentItems, setCurrentItems] = useState([])
-  const [pageCount, setPageCount] = useState(0)
-  const [itemOffset, setItemOffset] = useState(0)
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
 
+  useEffect(async () => {
+    async function fetchItems() {
+      const response = await fetch(`http://3.138.238.97:8080/availableNFB`);
+      const data = await response.json();
+      setItems(data);
+    }
+    await fetchItems();
+  });
 
   useEffect(() => {
-    const endOffset = itemOffset + itemsPerPage
-    setCurrentItems(items.slice(itemOffset, endOffset))
-    setPageCount(Math.ceil(items.length / itemsPerPage))
-  }, [itemOffset, itemsPerPage])
-
-
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(items.slice(itemOffset, endOffset));
+    console.log(currentItems);
+    setPageCount(Math.ceil(items.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage]);
 
   const handlePageClick = (e) => {
     const newOffset = (e.selected * itemsPerPage) % items.length;
-    setItemOffset(newOffset)
-  }
+    setItemOffset(newOffset);
+  };
 
   return (
     <>
-      <div className=''>
+      <div className="">
         <ReactPaginate
-          breakLabel='...'
+          breakLabel="..."
           pageCount={pageCount}
           pageRangeDisplayed={5}
           onPageChange={handlePageClick}
-          className='text-white flex justify-center space-x-2 items-center sm:space-x-[3px] '
-          previousLinkClassName='bg-teal-200 text-blue-900 py-1 px-2 rounded-lg sm:text-sm font-bold hover:bg-teal-300'
-          nextLinkClassName='bg-teal-200 text-blue-900 px-2 py-1 rounded-lg sm:text-sm font-bold hover:bg-teal-300'
-          pageLinkClassName='bg-white text-blue-800 px-2 py-1 rounded sm:px-1 sm:text-sm sm:hidden hover:bg-gray-600 hover:text-white'
+          className="text-white flex justify-center space-x-2 items-center sm:space-x-[3px] "
+          previousLinkClassName="bg-teal-200 text-blue-900 py-1 px-2 rounded-lg sm:text-sm font-bold hover:bg-teal-300"
+          nextLinkClassName="bg-teal-200 text-blue-900 px-2 py-1 rounded-lg sm:text-sm font-bold hover:bg-teal-300"
+          pageLinkClassName="bg-white text-blue-800 px-2 py-1 rounded sm:px-1 sm:text-sm sm:hidden hover:bg-gray-600 hover:text-white"
           renderOnZeroPageCount={null}
         />
       </div>
-      <div className='text-white grid grid-cols-4 lg:grid-cols-2 justify-center xl:grid-cols-3 sm:grid-cols-1'>
-        {currentItems.map((e,i) => {
-          {/* Send data according to the structure of data in the given variables */ }
+      <div className="text-white grid grid-cols-4 lg:grid-cols-2 justify-center xl:grid-cols-3 sm:grid-cols-1">
+        {currentItems.map((e, i) => {
+          {
+            /* Send data according to the structure of data in the given variables */
+          }
           return (
             <div key={i}>
-            <Card img={e.img} price={e.price} band={e.band} time={e.time} button={e.button}/>
+              <Card
+                img={e.image == "Dark" ? Dark : Light}
+                price={e.price}
+                bond={e.bondId}
+                time={e.timestamp}
+                button="BUY"
+              />
             </div>
-          )
+          );
         })}
       </div>
     </>
-  )
+  );
 }
--- src/components/Sales/Card.js
+++ src/components/Sales/Card.js
@@ -1,25 +1,29 @@
-import React from 'react'
-import certificate from '../../Media/Cartificate-Dark.png'
-import ModalEl from './ModalEl'
-import ModalElSold from './ModalElSold'
+import React from "react";
+import certificate from "../../Media/Cartificate-Dark.png";
+import ModalEl from "./ModalEl";
+import ModalElSold from "./ModalElSold";
 
 export default function Card(props) {
-    return (
-        <div className='w-[350px] 3xl:w-[330px] 2xl:w-[270px] lg:w-[260px] sm:w-[260px] mx-auto'>
-            <div className='bg-[#1d1236]  px-5 rounded-lg mb-10 '>
-                <img src={props.img} className='pt-6 pb-2' />
-                <div className='flex justify-between items-center'>
-                    <p className='text-teal-200 text-xl font-bold lg:text-base sm:text-sm'>{props.price}</p>
-                    <p className='font-bold text-sm lg:text-xs'>{`Band ${props.band}`}</p>
-                </div>
-                <div className='flex items-center justify-between py-4 pb-8'>
-                    <p className='text-xs '>{props.time}</p>
-                    
-                    {props.button==='BUY'?<ModalEl name={props.button}/>:<ModalElSold name={props.button}/>}
-                </div>
-
-            </div>
-
+  return (
+    <div className="w-[350px] 3xl:w-[330px] 2xl:w-[270px] lg:w-[260px] sm:w-[260px] mx-auto">
+      <div className="bg-[#1d1236]  px-5 rounded-lg mb-10 ">
+        <img src={props.img} className="pt-6 pb-2" />
+        <div className="flex justify-between items-center">
+          <p className="text-teal-200 text-xl font-bold lg:text-base sm:text-sm">
+            {props.price}
+          </p>
+          <p className="font-bold text-sm lg:text-xs">{`Band ${props.bond}`}</p>
         </div>
-    )
+        <div className="flex items-center justify-between py-4 pb-8">
+          <p className="text-xs ">{props.time}</p>
+
+          {props.button === "BUY" ? (
+            <ModalEl name={props.button} />
+          ) : (
+            <ModalElSold name={props.button} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
