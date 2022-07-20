a08b3cd4a20c130fbb0f1f9bca882643d2295517
pratikshit vashistha <50011440+pratikshitvas@users.noreply.github.com>
2022-07-20 22:50:00 +0530

    Update BannerW.js

--- src/components/BannerW.js
+++ src/components/BannerW.js
@@ -5,10 +5,10 @@ export default function BannerW() {
   return (
     <div className='flex justify-center items-center flex-wrap'>
         <div className='flex flex-col items-start flex-wrap ml-10 mt-10'>
-            <p className='text-white text-5xl font-semibold'>The <span className='text-teal-400'>Future</span> of finance</p>
+            <p className='text-white text-5xl font-semibold'>The <span className='italic text-teal-400'>Future</span> of finance</p>
             <p className='text-white text-5xl font-semibold'>is Non-Fungible</p>
             <p className='text-white'>The official digital financial product fro CD3Fi & CinemaDraft</p>
-            <button className=' bg-teal-400 text-blue-900 font-semibold px-9 py-3 rounded-xl hover:bg-teal-200'>GET YOUR NFB</button>
+            <button className='px-10 py-2 rounded-lg hover:bg-teal-500 bg-teal-200 text-black font-semibold' >GET YOUR NFB</button>
         </div>
         <div>
             <img src={nfb}/>
