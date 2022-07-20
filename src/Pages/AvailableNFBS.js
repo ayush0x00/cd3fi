17b53c1eb50de1e8c5bbed4834864599fc48fd78
Ayush Tiwari <ayushtiwari5747462@gmail.com>
2022-07-20 15:05:59 +0530

    API calls added

--- src/Pages/AvailableNFBS.js
+++ src/Pages/AvailableNFBS.js
@@ -10,39 +10,28 @@ export default function AvailableNFBS() {
   {
     /* Import your own fetched data for the items */
   }
-  const [items, setItems] = useState([{}]);
-  const [loading, setLoading] = useState(true);
 
+  const [items, setitems] = useState([]);
   const itemsPerPage = 8;
   const [currentItems, setCurrentItems] = useState([]);
   const [pageCount, setPageCount] = useState(0);
   const [itemOffset, setItemOffset] = useState(0);
 
-  const initialize = async () => {
-    await fetchItems();
-    await generateOffset();
-  };
-
-  const fetchItems = async () => {
-    const response = await fetch(`http://3.138.238.97:8080/availableNFB`);
-    const data = await response.json();
-    setItems(data);
-    setLoading(false);
-    console.log("Loading is false");
-    console.log(items);
-  };
-
   useEffect(() => {
-    fetchItems();
-    generateOffset();
-  }, [loading]);
-
-  const generateOffset = () => {
+    const getData = async () => {
+      try {
+        const fetchApi = await fetch("http://3.138.238.97:8080/availableNFB");
+        const datas = await fetchApi.json();
+        setitems(datas);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
     const endOffset = itemOffset + itemsPerPage;
     setCurrentItems(items.slice(itemOffset, endOffset));
-    console.log(currentItems);
     setPageCount(Math.ceil(items.length / itemsPerPage));
-  };
+  }, [itemOffset, itemsPerPage, items.length]);
 
   const handlePageClick = (e) => {
     const newOffset = (e.selected * itemsPerPage) % items.length;
@@ -51,44 +40,38 @@ export default function AvailableNFBS() {
 
   return (
     <>
-      {console.log(loading)}
-      {loading === true ? (
-        <>{alert("Loading")}</>
-      ) : (
-        <>
-          <div className="">
-            <ReactPaginate
-              breakLabel="..."
-              pageCount={pageCount}
-              pageRangeDisplayed={5}
-              onPageChange={handlePageClick}
-              className="text-white flex justify-center space-x-2 items-center sm:space-x-[3px] "
-              previousLinkClassName="bg-teal-200 text-blue-900 py-1 px-2 rounded-lg sm:text-sm font-bold hover:bg-teal-300"
-              nextLinkClassName="bg-teal-200 text-blue-900 px-2 py-1 rounded-lg sm:text-sm font-bold hover:bg-teal-300"
-              pageLinkClassName="bg-white text-blue-800 px-2 py-1 rounded sm:px-1 sm:text-sm sm:hidden hover:bg-gray-600 hover:text-white"
-              renderOnZeroPageCount={null}
-            />
-          </div>
-          <div className="text-white grid grid-cols-4 lg:grid-cols-2 justify-center xl:grid-cols-3 sm:grid-cols-1">
-            {currentItems.map((e, i) => {
-              {
-                /* Send data according to the structure of data in the given variables */
-              }
-              return (
-                <div key={i}>
-                  <Card
-                    img={e.image == "Dark" ? Dark : Light}
-                    price={e.price}
-                    bond={e.bondId}
-                    time={e.timestamp}
-                    button="BUY"
-                  />
-                </div>
-              );
-            })}
-          </div>
-        </>
-      )}
+      <div className="">
+        <ReactPaginate
+          breakLabel="..."
+          pageCount={pageCount}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageClick}
+          className="text-white flex justify-center space-x-2 items-center sm:space-x-[3px] "
+          previousLinkClassName="bg-teal-200 text-blue-900 py-1 px-2 rounded-lg sm:text-sm font-bold hover:bg-teal-300"
+          nextLinkClassName="bg-teal-200 text-blue-900 px-2 py-1 rounded-lg sm:text-sm font-bold hover:bg-teal-300"
+          pageLinkClassName="bg-white text-blue-800 px-2 py-1 rounded sm:px-1 sm:text-sm sm:hidden hover:bg-gray-600 hover:text-white"
+          renderOnZeroPageCount={null}
+        />
+      </div>
+      <div className="text-white grid grid-cols-4 lg:grid-cols-2 justify-center xl:grid-cols-3 sm:grid-cols-1">
+        {items.length != 0 &&
+          currentItems.map((e, i) => {
+            {
+              /* Send data according to the structure of data in the given variables */
+            }
+            return (
+              <div key={i}>
+                <Card
+                  img={e.image == "Dark" ? Dark : Light}
+                  price={e.price}
+                  bond={e.bondId}
+                  time={e.timestamp}
+                  button="BUY"
+                />
+              </div>
+            );
+          })}
+      </div>
     </>
   );
 }
--- src/Pages/SoldNFBS.js
+++ src/Pages/SoldNFBS.js
@@ -1,59 +1,73 @@
-import React from 'react'
-import Card from '../components/Sales/Card'
-import certificate from '../Media/NFBCertificate.png'
-import { useState, useEffect } from 'react'
-import ReactPaginate from 'react-paginate'
-import { data } from '../fakeData'
+import React from "react";
+import Card from "../components/Sales/Card";
+import Dark from "../Media/Cartificate-Dark.png";
+import Light from "../Media/NFB Certificate (3)@2x.png";
+import { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
 
 export default function SoldNFBs() {
- 
   const itemsPerPage = 8;
-  const [currentItems, setCurrentItems] = useState([])
-  const [pageCount, setPageCount] = useState(0)
-  const [itemOffset, setItemOffset] = useState(0)
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
 
-
-  {/* Import your own fetched data for the items */}
-  const items = data
+  {
+    /* Import your own fetched data for the items */
+  }
+  const [items, setitems] = useState([]);
 
   useEffect(() => {
-    const endOffset = itemOffset + itemsPerPage
-    setCurrentItems(items.slice(itemOffset, endOffset))
-    setPageCount(Math.ceil(items.length / itemsPerPage))
-  }, [itemOffset, itemsPerPage])
-
-
+    const getData = async () => {
+      try {
+        const fetchApi = await fetch("http://3.138.238.97:8080/soldNFB");
+        const datas = await fetchApi.json();
+        setitems(datas);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(items.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(items.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, items.length]);
 
   const handlePageClick = (e) => {
     const newOffset = (e.selected * itemsPerPage) % items.length;
-    setItemOffset(newOffset)
-    
-  }
-
+    setItemOffset(newOffset);
+  };
 
   return (
-    <div className='flex flex-col '>
+    <div className="flex flex-col ">
       <ReactPaginate
-        breakLabel='...'
+        breakLabel="..."
         pageCount={pageCount}
         pageRangeDisplayed={5}
         onPageChange={handlePageClick}
-        className='text-white flex justify-center space-x-2 items-center sm:space-x-[3px] '
-        previousLinkClassName='bg-teal-200 text-blue-900 py-1 px-2 rounded-lg sm:text-sm font-bold hover:bg-teal-300'
-        nextLinkClassName='bg-teal-200 text-blue-900 px-2 py-1 rounded-lg sm:text-sm font-bold hover:bg-teal-300'
-        pageLinkClassName='bg-white text-blue-800 px-2 py-1 rounded sm:px-1 sm:text-sm sm:hidden hover:bg-gray-600 hover:text-white'
+        className="text-white flex justify-center space-x-2 items-center sm:space-x-[3px] "
+        previousLinkClassName="bg-teal-200 text-blue-900 py-1 px-2 rounded-lg sm:text-sm font-bold hover:bg-teal-300"
+        nextLinkClassName="bg-teal-200 text-blue-900 px-2 py-1 rounded-lg sm:text-sm font-bold hover:bg-teal-300"
+        pageLinkClassName="bg-white text-blue-800 px-2 py-1 rounded sm:px-1 sm:text-sm sm:hidden hover:bg-gray-600 hover:text-white"
         renderOnZeroPageCount={null}
       />
-      <div className='text-white grid grid-cols-4 lg:grid-cols-2 justify-center xl:grid-cols-3 sm:grid-cols-1'>
-        {currentItems.map((e,i)=>{
-          {/* Send data according to the structure of data in the given variables */}
-          return(
+      <div className="text-white grid grid-cols-4 lg:grid-cols-2 justify-center xl:grid-cols-3 sm:grid-cols-1">
+        {currentItems.map((e, i) => {
+          {
+            /* Send data according to the structure of data in the given variables */
+          }
+          return (
             <div key={i}>
-          <Card img={e.img} price={e.price} band={e.band} time={e.time} button={e.button} />
-          </div>
-          )
+              <Card
+                img={e.image == "Dark" ? Dark : Light}
+                price={e.price}
+                bond={e.bondId}
+                time={e.timestamp}
+                button="SOLD"
+              />
+            </div>
+          );
         })}
-
-      </div></div>
-  )
+      </div>
+    </div>
+  );
 }
