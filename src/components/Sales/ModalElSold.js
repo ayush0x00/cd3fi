591376a3e25dd8b17fa67a40393509d404eab8f8
pratikshit vashistha <50011440+pratikshitvas@users.noreply.github.com>
2022-07-26 12:31:33 +0530

    Update ModalElSold.js

--- src/components/Sales/ModalElSold.js
+++ src/components/Sales/ModalElSold.js
@@ -30,7 +30,7 @@ export default function ModalElSold(props) {
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay bg="rgba(0,0,0,0.45)" />
         <ModalContent
-          bg="#d4fcf2"
+          bg="#FFFFFF"
           w="450px"
           mx="auto"
           rounded="20px"
