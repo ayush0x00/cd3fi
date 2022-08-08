55e0c6242246e4d1a7a620a2c93ae2075e1023b2
pratikshit vashistha <50011440+pratikshitvas@users.noreply.github.com>
2022-08-08 21:46:40 +0530

    submmision

--- src/components/Sales/ModalBuy.js
+++ src/components/Sales/ModalBuy.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Modal,
     ModalOverlay,
@@ -9,39 +9,129 @@ import {
     ModalCloseButton,
     useDisclosure,
     Button,
+    Alert,
+    AlertIcon,
+    AlertTitle,
+    AlertDescription,
+    CloseButton,
+
 } from '@chakra-ui/react'
 import arrow from '../../static/images/Icon feather-arrow-up.png'
 
+
 export default function ModalBuy() {
     const { isOpen, onOpen, onClose } = useDisclosure()
-    return (
-        <div>
-            <Button onClick={onOpen} bg="#99EFFF" height="50px" width="120px" rounded="10px" fontWeight="semibold" >Buy </Button>
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay bg="rgba(0,0,0,0.7)" />
-                <ModalContent bg="#FFFFFF"
-                    w="450px"
-                    mx="auto"
-                    rounded="20px"
-                    height="350px"
-                    px="20px"
-                    pt="30px"
-                    zIndex={10}
-                    mt="200px" >
-                    <ModalCloseButton ml="auto" color="white" />
-                    < ModalBody >
-                        <div className='flex flex-col items-center'>
-                            <img src={arrow} className='py-10'/>
-                            <p className='font-semibold text-lg'>Submitting Transaction</p>
-                            <p className='text-sm'>View on Binance</p>
+    const [mod, setmod] = useState(true)
+    return ( <
+            div >
 
-                        </div>
-                    </ModalBody>
-                    < ModalFooter  >
-                        <button className='mx-auto w-[300px] bg-[#99EFFF] font-semibold rounded-lg py-2 text-sm' onClick={onClose} >Close</button>
-                    </ModalFooter >
-                </ModalContent>
-            </Modal>
-        </div>
-    )
-}
+            <
+            Button onClick = {
+                () => {
+                    onOpen()
+
+                }
+            }
+            bg = "#99EFFF"
+            height = "50px"
+            width = "120px"
+            rounded = "10px"
+            fontWeight = "semibold" > Buy < /Button> <
+            Modal isOpen = { isOpen }
+            onClose = { onClose } >
+            <
+            ModalOverlay bg = "rgba(0,0,0,0.7)" / >
+            <
+            ModalContent bg = "#FFFFFF"
+            w = "450px"
+            mx = "auto"
+            rounded = "20px"
+            height = "350px"
+            px = "20px"
+            pt = "30px"
+            zIndex = { 10 }
+            mt = "200px" >
+            <
+            ModalCloseButton ml = "auto"
+            color = "white" / >
+            <
+            ModalBody >
+            <
+            div className = 'absolute -top-48 left-[30%]' >
+            <
+            Alert status = 'success'
+            variant = 'subtle'
+            flexDirection = 'column'
+            alignItems = 'center'
+            justifyContent = 'center'
+            textAlign = 'center'
+            height = '200px'
+
+            bg = 'red'
+
+            >
+            <
+            AlertIcon boxSize = '40px'
+            mr = { 0 }
+            /> <
+            AlertTitle mt = { 4 }
+            mb = { 1 }
+            fontSize = 'lg' >
+            Application submitted!
+            <
+            /AlertTitle> <
+            AlertDescription maxWidth = 'sm' >
+            <
+            div className = 'space-x-6' >
+            <
+            button className = 'bg-white text-black w-20 rounded'
+            onClick = {
+                () => { onClose() } } > No < /button> <
+            button className = 'bg-white text-black w-20 rounded'
+            onClick = {
+                () => { setmod(false) } } > yes < /button> <
+            /div> <
+            /AlertDescription>
+
+            <
+            /Alert></div > {
+                mod && < div className = 'flex flex-col items-center' >
+                <
+                img src = { arrow }
+                className = 'py-10' / >
+                <
+                p className = 'font-semibold text-lg' > Submitting Transaction < /p> <
+                p className = 'text-sm' > View on Binance < /p>
+
+                <
+                /div>} {
+                    !mod && < div className = 'flex flex-col items-center' >
+                        <
+                        img src = { arrow }
+                    className = 'py-10' / >
+                        <
+                        p className = 'font-semibold text-lg' > Transaction Submitted < /p> <
+                        p className = 'text-sm' > View on Binance < /p>
+
+                    <
+                    /div>} <
+                    /ModalBody>
+
+                    <
+                    ModalFooter >
+                        <
+                        button className = 'mx-auto w-[300px] bg-[#99EFFF] font-semibold rounded-lg py-2 text-sm'
+                    onClick = {
+                            () => {
+                                onClose()
+                                setmod(true)
+                            }
+                        } > Close < /button> <
+                        /ModalFooter > <
+                        /ModalContent> <
+                        /Modal>
+
+                    <
+                    /div>
+                )
+            }
