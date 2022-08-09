476752b9d2792380f276d05a962e57e77df4ff04
Pratikshit Vashistha <50011440+pratikshitvas@users.noreply.github.com>
2022-08-09 13:39:59 +0530

    Update InputComp.js

--- src/components/InputComp.js
+++ src/components/InputComp.js
@@ -4,8 +4,8 @@ export default function InputComp() {
     return ( <
         div className = 'flex justify-center flex-col items-center' >
         <
-        p className = 'text-white text-2xl' > 'They say money can’t buy happiness but it sure as hell buys everything else' < /p> <
-        p className = 'text-[#99efff] text-lg' > -MAD MONEY < /p> <
+        p className = 'text-white text-2xl' > "They say money can’t buy happiness but it sure as hell buys everything else" < /p> <
+        p className = 'text-[#99efff] text-lg' > - MAD MONEY < /p> <
         div className = 'flex bg-[#1d1236] py-2 px-4 rounded-xl' >
         <
         form action = "https://cinemadraft.us14.list-manage.com/subscribe/post?u=fcd503faf1285a57eea370664&amp;id=e3718094a2"
